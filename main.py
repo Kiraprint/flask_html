9f43d2754c7e4598ea5325dd45bed52c99a6a5f6
Kiraprint <76822921+Kiraprint@users.noreply.github.com>
2021-04-23 12:40:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('base.html', title='yep')
+@app.route('/<title>')
+def base(title):
+    return render_template('base.html', title=title, css=url_for('static', filename='css/style.css'))
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof, img=url_for('static', filename='img/scheme.png'))
+
+
+@app.route('/list_prof/<key>')
+def list_prof(key):
+    profs = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог',
+             'врач', 'инженер по терраформированию',
+             'климатолог', 'специалист по радиационной защите',
+             'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+             'метеоролог', 'оператор марсохода', 'киберинженер',
+             'штурман', 'пилот дронов']
+    return render_template('list_prof.html', key=key, profs=profs)
 
 
 if __name__ == '__main__':
